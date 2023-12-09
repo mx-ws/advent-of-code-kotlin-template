668aad8e978f4206ba0414e6bc24f5f2a55556f7
max <mwilkens@protonmail.ch>
2023-12-09 22:41:40 +0100

    Day 06 Part 02

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,37 +1,40 @@
 fun main() {
-    fun part1line(time: Int, distance: Int): Int {
-        return (0..time)
+    fun part1line(time: ULong, distance: ULong): ULong{
+        return (0uL..time)
             .map { it * (time - it) }
             .filter { it > distance }
             .size
+            .toULong()
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): ULong {
         return input
             .map { line ->
                 line.dropWhile { !it.isDigit() }
                     .split(" ")
-                    .mapNotNull { it.toIntOrNull() }
+                    .mapNotNull { it.toULongOrNull() }
             }
             .let { it[0].zip(it[1]) }
-            .fold(1) { acc, (time, distance) ->
+            .fold(1uL) { acc, (time, distance) ->
                 part1line(time, distance) * acc
             }
     }
 
-    fun part2(input: List<String>): Int {
-        return 5
+    fun part2(input: List<String>): ULong {
+        return input
+            .map { line -> line.filter { it.isDigit() }.toULong() }
+            .let { (time, distance) -> part1line(time, distance) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     println(part1(testInput))
-    check(part1(testInput) == 288)
+    check(part1(testInput) == 288uL)
     println(part2(testInput))
-    // check(part2(testInput) == 2)
+    check(part2(testInput) == 71503uL)
 
     val input = readInput("Day06")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
 
