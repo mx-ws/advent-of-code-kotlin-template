13e2dcf6a7d7ae3a7754ce8aa05a77b51989866e
max <mwilkens@protonmail.ch>
2023-12-09 15:40:23 +0100

    Day 09 Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -8,13 +8,20 @@ fun main() {
         return diff
     }
 
-    fun part1line(line: List<Int>): Int {
+    fun getTransformedLines(line: List<Int>): Pair<List<List<Int>>, Int> {
         val transformedLines = mutableListOf(line)
         var i = 0
         while (transformedLines[i].any { it != 0 }) {
             transformedLines.add(diffLine(transformedLines[i]))
             i += 1
         }
+
+        return transformedLines to i
+    }
+
+    fun part1line(line: List<Int>): Int {
+        val (transformedLines, k) = getTransformedLines(line)
+        var i = k
         var last = 0
         while (i > 0) {
             last += transformedLines[i - 1].last()
@@ -29,17 +36,31 @@ fun main() {
             .sumOf { line -> part1line(line.split(" ").map { it.toInt() }) }
     }
 
+    fun part2line(line: List<Int>): Int {
+        val (transformedLines, k) = getTransformedLines(line)
+        var i = k
+        var first = 0
+        while (i > 0) {
+            first = transformedLines[i - 1].first() - first
+            i -= 1
+        }
+
+        return first
+    }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .sumOf { line -> part2line(line.split(" ").map { it.toInt() }) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
     println(part1(testInput))
-//    check(part1(testInput) == 114)
+    check(part1(testInput) == 114)
+    println(part2(testInput))
+    check(part2(testInput) == 2)
 
     val input = readInput("Day09")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
